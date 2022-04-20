d28952f8b1187eedb66ea3e3f5432b45bf98bea6
Ash <ashsingh1790@users.noreply.github.com>
2022-04-20 17:53:00 -0500

    test: Snapshot updates.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
 test('renders App', () => {
-  render(<App />)
-  const hello = screen.getByText(/clear site/i)
-  expect(hello).toBeInTheDocument()
+  const { asFragment } = render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  )
+  expect(asFragment()).toMatchSnapshot()
 })
