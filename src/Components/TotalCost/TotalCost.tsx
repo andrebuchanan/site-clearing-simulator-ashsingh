e73eecefab6f54dd9fa543cc47d65adc9de902a5
Ash <ashsingh1790@users.noreply.github.com>
2022-04-19 20:02:42 -0500

    fix(Cost): Total Cost calculation.

--- src/Components/TotalCost/TotalCost.tsx
+++ src/Components/TotalCost/TotalCost.tsx
@@ -5,8 +5,21 @@ import Typography from '@mui/material/Typography'
 import { AppContext } from '../../Context'
 
 export const TotalCost = () => {
-  const { itemizedCosts } = useContext(AppContext)
-  const totalCost = itemizedCosts.reduce((acc, i) => acc + i, 0)
+  const { itemizedCosts, sessionState, siteData } = useContext(AppContext)
+
+  // calculate cost of visited squares
+  let totalCost = itemizedCosts.reduce((acc, i) => acc + i, 0)
+
+  // calculate cost of uncleared squares
+  if (sessionState.quit) {
+    for (let i = 0; i < siteData.length; i += 1) {
+      for (let j = 0; j < siteData[0].length; j += 1) {
+        const terrainType = siteData[i][j]
+        if (terrainType === 'r' || terrainType === 't') totalCost += 3
+      }
+    }
+  }
+
   return (
     <Typography variant="h6" component="div" gutterBottom>
       Total Cost: {totalCost} Credits
