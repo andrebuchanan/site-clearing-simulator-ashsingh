3c15abfb36a364426ed79192d5291c247cb7c67c
Ash <ashsingh1790@users.noreply.github.com>
2022-04-20 20:09:31 -0500

    test: TotalCost tests.

--- src/Components/TotalCost/__tests__/TotalCosts.test.tsx
+++ src/Components/TotalCost/__tests__/TotalCosts.test.tsx
@@ -5,7 +5,12 @@ import { AppContext } from '../../../Context'
 import TotalCost from '../TotalCost'
 
 const contextValue = {
-  itemizedCosts: [1, 5, 3]
+  itemizedCosts: [1, 5, 3],
+  sessionState: { quit: false },
+  siteData: [
+    ['o', 't', 't'],
+    ['o', 'o', 't']
+  ]
 }
 
 test('renders TotalCost', () => {
@@ -17,4 +22,16 @@ test('renders TotalCost', () => {
 
   expect(asFragment()).toMatchSnapshot()
   expect(screen.getByText(/9 Credits/i)).toBeDefined()
+})
+
+test('handles quit state', () => {
+  const { asFragment } = render(
+    <AppContext.Provider
+      value={{ ...contextValue, sessionState: { quit: true } }}
+    >
+      <TotalCost />
+    </AppContext.Provider>
+  )
+
+  expect(screen.getByText(/18 Credits/i)).toBeDefined()
 })
