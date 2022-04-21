e6086a4d53db3e3999d0410e8eafeead80da2445
Ash <ashsingh1790@users.noreply.github.com>
2022-04-20 20:02:23 -0500

    test: QuitDialog tests.

--- src/Components/QuitDialog/__tests__/QuitDialog.test.tsx
+++ src/Components/QuitDialog/__tests__/QuitDialog.test.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import { render, screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 
@@ -13,31 +14,37 @@ const contextValue = {
 describe('Quit Dialog', () => {
   it('renders correctly', () => {
     const { asFragment } = render(
-      <AppContext.Provider value={contextValue}>
-        <QuitDialog />
-      </AppContext.Provider>
+      <BrowserRouter>
+        <AppContext.Provider value={contextValue}>
+          <QuitDialog />
+        </AppContext.Provider>
+      </BrowserRouter>
     )
     expect(asFragment()).toMatchSnapshot()
   })
 
   it('renders dialog', () => {
     render(
-      <AppContext.Provider
-        value={{ ...contextValue, sessionState: { quit: true } }}
-      >
-        <QuitDialog />
-      </AppContext.Provider>
+      <BrowserRouter>
+        <AppContext.Provider
+          value={{ ...contextValue, sessionState: { quit: true } }}
+        >
+          <QuitDialog />
+        </AppContext.Provider>
+      </BrowserRouter>
     )
     expect(screen.getByText(/Your session has ended/i)).toBeVisible()
   })
 
   it('closes dialog', async () => {
     render(
-      <AppContext.Provider
-        value={{ ...contextValue, sessionState: { quit: true } }}
-      >
-        <QuitDialog />
-      </AppContext.Provider>
+      <BrowserRouter>
+        <AppContext.Provider
+          value={{ ...contextValue, sessionState: { quit: true } }}
+        >
+          <QuitDialog />
+        </AppContext.Provider>
+      </BrowserRouter>
     )
     await waitFor(() => userEvent.click(screen.getByTestId('close-dialog')))
     expect(screen.getByText(/Your session has ended/i)).not.toBeVisible()
